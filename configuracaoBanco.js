c708985903992c2df4b5e4df2ea6ae7940d29f7a
juniorLG-dev <contaproxadrez2023@gmail.com>
2026-04-20 18:55:46 -0300

    refactor: hide default values

--- configuracaoBanco.js
+++ configuracaoBanco.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 const credenciais = {
-    user: process.env.DB_USER || 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'scleenkr',
-    password: process.env.DB_PASSWORD || '48344834',
-    port: process.env.DB_PORT || 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 };
 console.log(credenciais.password)
 module.exports = credenciais;
