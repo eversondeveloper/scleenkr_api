c6e78b1869ac2637069d8d0190fb9e912d5b1aab
juniorLG-dev <contaproxadrez2023@gmail.com>
2026-04-28 19:25:11 -0300

    chore: refatoração no monolito para não misturar modulos entre branches

--- src/modules/retiradas/retiradas.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const controller = require('./retiradas.controller');
-
-const router = Router();
-
-router.get('/',  controller.listar);
-router.post('/', controller.criar);
-
-module.exports = router;
