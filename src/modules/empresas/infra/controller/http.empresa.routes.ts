dec77d4eaee64d086b1e972aba72e9fc17442c1b
juniorLG-dev <contaproxadrez2023@gmail.com>
2026-05-07 00:49:47 -0300

    feat: cria rotas http

---
+++ src/modules/empresas/infra/controller/http.empresa.routes.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express'
+import { HttpEmpresaController } from './http.empresa.controller'
+import { manipuladorAssincrono } from '@/middlewares/async.handler'
+
+export class RotasEmpresas {
+    private readonly router: Router
+    constructor(private readonly controller: HttpEmpresaController) {
+        this.router = Router()
+        this.iniciarRotas()
+    }
+    private iniciarRotas(): void {
+        this.router.post(
+            "/empresa",
+            manipuladorAssincrono(
+                this.controller.criarEmpresaHandler.bind(this.controller)
+            )
+        )
+        this.router.put(
+            "/empresa/:id",
+            manipuladorAssincrono(
+                this.controller.atualizarInscricaoEstadualHandler.bind(this.controller)
+            )
+        )
+        this.router.get(
+            "/empresa/:id",
+            manipuladorAssincrono(
+                this.controller.buscarEmpresaPorIDHandler.bind(this.controller)
+            )
+        )
+    }
+
+    public getRouter(): Router {
+        return this.router;
+    }
+}
